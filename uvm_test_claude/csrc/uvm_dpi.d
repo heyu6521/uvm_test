b06eb9d79e3b3f844889a1fbcfedb3415327f215
Example User <huigubuzhichunqiu@gmail.com>
2025-04-29 11:17:56 +0800

    delete

--- uvm_test_claude/csrc/uvm_dpi.d
+++
@@ -1,9 +0,0 @@
-uvm_dpi.o: /opt/synopsys/vcs/vcs-mx/O-2018.09-SP2/etc/uvm/dpi/uvm_dpi.cc \
- /opt/synopsys/vcs/vcs-mx/O-2018.09-SP2/etc/uvm/dpi/uvm_regex.cc \
- /opt/synopsys/vcs/vcs-mx/O-2018.09-SP2/include/vpi_user.h \
- /opt/synopsys/vcs/vcs-mx/O-2018.09-SP2/etc/uvm/dpi/uvm_hdl.c \
- /opt/synopsys/vcs/vcs-mx/O-2018.09-SP2/include/sv_vpi_user.h \
- /opt/synopsys/vcs/vcs-mx/O-2018.09-SP2/include/vpi_user.h \
- /opt/synopsys/vcs/vcs-mx/O-2018.09-SP2/include/veriuser.h \
- /opt/synopsys/vcs/vcs-mx/O-2018.09-SP2/include/svdpi.h \
- /opt/synopsys/vcs/vcs-mx/O-2018.09-SP2/etc/uvm/dpi/uvm_svcmd_dpi.c
